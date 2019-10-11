488843478fc3c5d941abdc1b23221799e97f366b
Dmytrozn <Dmytro.zn.gmail.com>
2019-10-11 13:24:52 +0300

    change decorator

--- lesson_3.py
+++ lesson_3.py
@@ -4,28 +4,23 @@ import time
 # Which will call a function a certain number of times, 
 # it will output the amount of time spent to perform 
 # this function and its name.
+
 def my_decorator(num=1):
 
     def decorator(func):
-        
 
         def wraper(*args, **kwargs):
             list_of_already = []
-            n = 1
             start_time = time.time()
-            while num >= n:
-                dict_of_val = {}
-                list_of_func = []
-                for i in range(n):
-                    list_of_func.append(func(*args, **kwargs))
-                dict_of_val['name of function'] = func.__name__
-                dict_of_val['out put'] = list_of_func
+            dict_of_val = {}
+            list_of_func = []
+            for i in range(num):
                 dict_of_val['time'] = f'{time.time() - start_time} sec'
+                dict_of_val['name of function'] = func.__name__
+                dict_of_val['out put'] = func(*args, **kwargs)
                 list_of_already.append(dict_of_val)  
-                n += 1    
-                        
-            return list_of_already
-
+                print(dict_of_val)
+            
         return wraper  
 
     return decorator
@@ -35,6 +30,41 @@ def say_hello(name):
     return f'hello {name}'
 
 print(say_hello('Dima'))
+
+
+
+
+# def my_decorator(num=1):
+
+#     def decorator(func):
+        
+
+#         def wraper(*args, **kwargs):
+#             list_of_already = []
+#             n = 1
+#             start_time = time.time()
+#             while num >= n:
+#                 dict_of_val = {}
+#                 list_of_func = []
+#                 for i in range(n):
+#                     list_of_func.append(func(*args, **kwargs))
+#                 dict_of_val['name of function'] = func.__name__
+#                 dict_of_val['out put'] = list_of_func
+#                 dict_of_val['time'] = f'{time.time() - start_time} sec'
+#                 list_of_already.append(dict_of_val)  
+#                 n += 1    
+                        
+#             return list_of_already
+
+#         return wraper  
+
+#     return decorator
+
+# @my_decorator(4)
+# def say_hello(name):
+#     return f'hello {name}'
+
+# print(say_hello('Dima'))
 
 
 
