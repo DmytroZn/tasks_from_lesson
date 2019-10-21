8e7834973b7c395afdc061dff70496b045c1b697
Dmytrozn <Dmytro.zn.gmail.com>
2019-10-21 17:10:04 +0300

    change file

--- lesson_4-2.py
+++ lesson_4-2.py
@@ -18,13 +18,12 @@ class Registration:
                     break
                 else:
                     print('\n' 'You should write only letters')
-            while True:
 
+            while True:
                 self._user_login = str(input('what is your login: '))
                 if re.match(r'\D', self._user_login):             
                     if self._user_login in f:
                         print('\n' 'This is login basy, try again')
-                    
                     else:
                         break
                 else:
@@ -36,7 +35,6 @@ class Registration:
                 if re.findall(r'([a-zA-Z]+)([0-9]+)', self._user_password_1):
                     if self._user_password_1 == self._user_password_2:
                         f[self._user_login] = [self._user_name, self._user_password_1, [f'{self._user_name}, you are registered - {time.ctime()}']]
-                        f['Admin'] = ['Admin', 'admin123',  'Hello, you are administrator']
                         print(f'Hello {self._user_name}, you are registered')
                         break
                     else:
@@ -50,7 +48,6 @@ class Authorization(Registration):
     LIST_RE = []
 
     def log_in(self):
-
         with shelve.open('file_of_data') as f:
             while True:
                 self._user_login = str(input('what is your login: '))
@@ -63,7 +60,7 @@ class Authorization(Registration):
             while True:
                 self._user_password_1 = str(input('write your password: '))
                 if f[self._user_login][1] == self._user_password_1:
-                    print(f'{f[self._user_login][0]}, You inside')
+                    
                     self.LIST_RE.insert(0, self._user_login)
                     try:
                         del self.LIST_RE[1]
@@ -72,27 +69,16 @@ class Authorization(Registration):
                     break
                 else:
                     print('Not correct password')
-                
+
+
 class Authorization_Admin(Authorization):
 
     def print_info(self):
         with shelve.open('file_of_data') as f:
             for k, v in f.items():
-                print(k, v)
-
-    
-        
-    # f['Admin'] = ['Admin', 'admin123',  'Hello, you are administrator']
-
-
-
-
-
-
-
-    
-
-
+                if k != 'Admin':
+                    print('\n' f'name: {v[0]}; login: {k}; \n posts: {v[2:]}')
+            
 
 class Posts:
 
@@ -110,28 +96,56 @@ class User(Authorization):
         self._a = '1 - sign_up'
         self._b = '2 - log_in'
         self._c = '3 - log in like admin'
+        self._d = '1 - see users'
+        self._e = '2 - log_out'
+        self._f = '1 - create post'
+        self._g = '3 - see post'
 
         while True:
-            print('\n'f'{self._a}, {self._b}')
+            print('\n'f'{self._a}, {self._b}, {self._c}')
             try:
                 choice = int(input('write your choice: '))
             except ValueError:
                 print('Write only \'1\' or \'2\'')
                 choice = 0
 
-
             if choice == 1:
                 Registration().sign_up()
+
             elif choice == 2:
+                with shelve.open('file_of_data') as f:
                     Authorization().log_in()
-                    print('\n'f'{self.LIST_RE[0]}, what would you like to do...?')
+                    self._name = self.LIST_RE[0]
+                    self._name = f[self._name][0]
+                    print(f'{self._name}, You inside')
+                    print('\n'f'{self._name}, what would you like to do...?')
                     break
-            
-        self._a = '1 - create post'
-        self._b = '2 - see post'
-        self._c = '3 - log_out'
+
+            elif choice == 3:
+                with shelve.open('file_of_data') as f:
+                    Authorization().log_in()
+                    self._name = self.LIST_RE[0]
+                    self._name = f[self._name][0]
+                    if self.LIST_RE[0] == 'Admin':
+                        print(f'{self._name}, You inside')
+                        print('\n'f'{self._name}, what would you like to do...?')
+                        while True:
+                            
+                            print('\n'f'{self._d}, {self._e}')
+                            try:
+                                choice = int(input('write your choice: '))
+                            except ValueError:
+                                print('Write only \'1\' or \'2\'')
+                                choice = 0
+                            if choice == 1:
+                                Authorization_Admin().print_info()
+                            elif choice == 2:
+                                break
+                    else:
+                        print('\n' f'{self._name}, You are not admin')
+                        
         while True:
-            print('\n' f'{self._a}, {self._b}, {self._c}')
+            print('\n' f'{self._f}, {self._e}, {self._g}')
             try:
                 choice = int(input('write your choice: '))
             except ValueError:
@@ -140,50 +154,29 @@ class User(Authorization):
             
             if choice == 1:
                 with shelve.open('file_of_data', 's') as f:
-               
-
                     var_of_log = self.LIST_RE[0]
                     list_of_log = f[var_of_log]
                     a=Posts().create_post()
                     list_of_log.append(a)
                     f[var_of_log] = list_of_log
-                    
-
-                
-
+        
             elif choice == 2:
-                with shelve.open('file_of_data') as f:
-
-                    # User().print_info()
-                 
-                    print( f[ self.LIST_RE[0] ] [2:] )
-                
+                break
 
             elif choice == 3:
-                break
-                        
+                with shelve.open('file_of_data') as f:
+                    print( f[ self.LIST_RE[0] ] [2:] )
+            
 
+class Machine():
 
-
-
-
-
-# Registration().sign_up()
-# Authorization().print_info()
-# Authorization().log_in()
-# one = User()
-# one.in_and_out()
-
-class A():
-    def do_food(self):
+    def do_work(self):
+        with shelve.open('file_of_data') as f:
+            f['Admin'] = ['Administrator', 'admin123',  'Hello, you are administrator']
         while True:
             one = User()
             one.enter()
 
 
-A().do_food()
+Machine().do_work()
 
-# one = Authorization()
-# one.enter()
-# one._user_login
-# one.
